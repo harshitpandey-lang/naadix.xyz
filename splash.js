8838ffbbc1f2e71c8f0e9a947fb8048ebe6bc7f2
Harshit Pandey <naadix.ind@gmail.com>
2026-03-06 12:51:05 +0530

    h

--- splash.js
+++ splash.js
@@ -1,7 +1,5 @@
-const logo = document.getElementById("logo");
-window.addEventListener("scroll", function() {
-  // Compute a scale factor based on how far the user has scrolled
-  const scrollY = window.scrollY;
-  const scale = 1 + scrollY / 300;
-  logo.style.transform = "scale(" + scale + ")";
-});
+setTimeout(function(){
+
+window.location.href="home.html";
+
+},4000);
