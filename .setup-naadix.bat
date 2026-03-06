619df884279f4b2a62d84f9a8ae6bd781a22217a
Harshit Pandey <naadix.ind@gmail.com>
2026-03-06 18:12:35 +0530

    added js

---
+++ .setup-naadix.bat
@@ -0,0 +1,54 @@
+echo Creating JavaScript structure...
+
+mkdir javascript
+mkdir javascript\products
+mkdir javascript\founder
+mkdir javascript\founder\projects
+mkdir javascript\lab
+mkdir javascript\lab\founder-dashboard
+mkdir javascript\lab\family-dashboard
+mkdir javascript\lab\guest-dashboard
+
+type nul > javascript\index.js
+type nul > javascript\home.js
+type nul > javascript\mission.js
+type nul > javascript\contact-us.js
+type nul > javascript\FAQ.js
+
+type nul > javascript\products\affiliate-agent.js
+type nul > javascript\products\content-gen-agent.js
+type nul > javascript\products\courses.js
+
+type nul > javascript\founder\intro.js
+type nul > javascript\founder\skills.js
+type nul > javascript\founder\event-gallery.js
+
+type nul > javascript\founder\projects\technocrew.js
+type nul > javascript\founder\projects\affiliate-agent.js
+type nul > javascript\founder\projects\content-gen-agent.js
+type nul > javascript\founder\projects\rover.js
+type nul > javascript\founder\projects\game.js
+type nul > javascript\founder\projects\website.js
+
+type nul > javascript\lab\home-lab.js
+
+type nul > javascript\lab\founder-dashboard\home-founder.js
+type nul > javascript\lab\founder-dashboard\calendar.js
+type nul > javascript\lab\founder-dashboard\vision.js
+type nul > javascript\lab\founder-dashboard\health.js
+type nul > javascript\lab\founder-dashboard\goals.js
+type nul > javascript\lab\founder-dashboard\education.js
+type nul > javascript\lab\founder-dashboard\finance.js
+type nul > javascript\lab\founder-dashboard\spiritual.js
+type nul > javascript\lab\founder-dashboard\tools.js
+
+type nul > javascript\lab\family-dashboard\home-family.js
+type nul > javascript\lab\family-dashboard\location.js
+type nul > javascript\lab\family-dashboard\academic-calendar.js
+
+type nul > javascript\lab\guest-dashboard\home-guest.js
+type nul > javascript\lab\guest-dashboard\location.js
+type nul > javascript\lab\guest-dashboard\personal-calendar.js
+type nul > javascript\lab\guest-dashboard\college-data.js
+
+echo JavaScript structure created!
