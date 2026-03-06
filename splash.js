7451ad4cedd11cb4f1bf24d51cd6792e37f01fe8
Harshit Pandey <naadix.ind@gmail.com>
2026-03-06 12:54:58 +0530

    i

--- splash.js
+++ splash.js
@@ -1,5 +1,35 @@
-setTimeout(function(){
+let logo = document.getElementById("logo");
+
+let triggered = false;
+
+window.addEventListener("wheel", function(event){
+
+if(triggered) return;
+
+if(event.deltaY > 0){
+
+triggered = true;
+
+/* zoom logo */
+
+logo.classList.add("zoom");
+
+/* fade screen */
+
+setTimeout(()=>{
+
+document.body.classList.add("fade");
+
+},900);
+
+/* go to next page */
+
+setTimeout(()=>{
 
 window.location.href="home.html";
 
-},4000);
+},1700);
+
+}
+
+});
