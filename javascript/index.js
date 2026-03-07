4f3f973ef33abeb28e5e770e158d6fe36a080229
Harshit Pandey <naadix.ind@gmail.com>
2026-03-07 06:00:14 +0530

    p

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,18 @@
 const logo = document.getElementById("logo");
 
-window.addEventListener("scroll", function() {
+// Logo scaling effect
+window.addEventListener("scroll", function () {
 
 const scrollY = window.scrollY;
-
 const scale = 1 + scrollY / 300;
 
 logo.style.transform = "scale(" + scale + ")";
+
+// Redirect to home page when user scrolls down
+if (scrollY > 150) {
+
+window.location.href = "webpages/home.html";
+
+}
 
 });
