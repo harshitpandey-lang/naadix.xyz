e5575ccf27174be3b9d5c32067a2b1db0da51c64
Harshit pandey <harshitpandey3519@gmail.com>
2026-09-13 17:30:18 +0530

    Point Founder HQ to new Supabase project

--- site/hq/config.js
+++ site/hq/config.js
@@ -1,5 +1,5 @@
 export const HQ_CONFIG = {
-  supabaseUrl: "https://bynkxhfzbityeufqllxi.supabase.co",
-  publishableKey: "sb_publishable_t2Q2WBFRw5nw-lztYRTP6g_Ig3Nq6DG",
+  supabaseUrl: "https://oruezuyfjavhjnadullk.supabase.co",
+  publishableKey: "sb_publishable_6NmScF3g4DFFAVs7QqFDNA_1ROCD5WO",
   founderEmail: "harshitpandey3519@gmail.com",
 };
